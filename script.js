214dd43240b10e7d036305339c92be97351c3efe
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2025-12-17 09:49:45 +0300

    portfolio
    The website's code has been updated to be a modern webpage.

--- script.js
+++ script.js
@@ -5,26 +5,19 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-// Footer yılını otomatik güncelle
 document.getElementById("year").textContent = new Date().getFullYear();
 
 function showProjectAlert() {
   alert("Proje detayları güncellenmektedir. Kodları Github'da inceleyebilirsiniz!");
 }
 
-// Scroll Animasyon Gözlemcisi
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('show'); // Görünür olduğunda sınıfı ekle
+      entry.target.classList.add('show');
     }
-    // Eğer yukarı çıkınca animasyonun tekrar etmesini istersen else bloğunu açabilirsin
-    // else {
-    //   entry.target.classList.remove('show');
-    // }
   });
 });
 
-// .hidden sınıfına sahip tüm elementleri seç ve gözlemle
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
